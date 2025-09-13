7ccaf1260e53e282ef84d87be4aa0f0270165f12
pankajgu <pankajgupta6362@gmail.com>
2025-09-13 18:46:01 +0530

    third changes

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         System.out.println("Hello world!");
 
         System.out.println("this is test class for git");
-        System.out.println("this is test class for git for second commit");
+        System.out.println("this is test class for git for second commit ");
 
     }
 }
